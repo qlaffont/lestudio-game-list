bb0e2ca67db51f8a7cbd3ed7ebdad6fa7433aa86
Quentin Laffont <quentin@flexper.fr>
2022-12-12 08:37:27 +0100

    refactor(app): use react instead of vue

---
+++ packages/renderer/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
